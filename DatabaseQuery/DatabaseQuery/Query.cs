0483894571ae087f855a3dd50d02aabd3959e6f5
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-01-22 14:04:53 +0100

    add folder structure / init db for sqlite

--- DatabaseQuery/DatabaseQuery/Query.cs
+++ DatabaseQuery/DatabaseQuery/Query.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 namespace DatabaseQuery
 {
     public class Query
     {
-
     }
 }
